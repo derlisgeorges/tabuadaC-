3b74bd61461f31b4f53a0a13c184395a42d7d511
DEV <dev@info.local>
2020-11-17 15:58:14 -0300

    taubada

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           for(int e = 1; e <= 10; e ++)
+           {
+               for( int d = 1; d<=10; d++)
+               {
+                   Console.WriteLine(e + "X" + d + " = " + e * d);
+               }
+               Console.WriteLine();
+           }
+        }
+
+    }
+}
